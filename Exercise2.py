2c80f237e6ae1a788fce37fb2a8697204ffd198a
KhatimBathspa <khatim2005@gmail.com>
2025-03-08 12:00:25 +0400

    Simple sums.py

--- Exercise2.py
+++ Exercise2.py
@@ -1,4 +1,4 @@
-firstnumber = 8    # Declaring and initializing the first variable
+firstnumber = 8    # Declaring and initializing the first variable 
 secondnumber = 10  # Declaring and initializing the second variable
 sumofnumbers = firstnumber + secondnumber  # Declaring the third variable and storing the sum of the first two numbers
 print(sumofnumbers)  # Print the value of the sum
